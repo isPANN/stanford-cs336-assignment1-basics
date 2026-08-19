bd49e0a2b7d45506cd0808a11cf73c5ab59abaa5
Xiwei Pan <isxiwei.pan@gmail.com>
2026-08-19 09:28:05 +0000

    update bpe

---
+++ cs336_basics/bpe.py
@@ -0,0 +1,10 @@
+import os
+
+def train_bpe(
+    input_path: str | os.PathLike,
+    vocab_size: int,
+    special_tokens: list[str],
+    **kwargs,
+) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
+    er
+    return vocab, merges
--- tests/adapters.py
+++ tests/adapters.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from cs336_basics.bpe import train_bpe
 
 import os
 from collections.abc import Iterable
@@ -589,4 +590,4 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    raise NotImplementedError
+    return train_bpe(input_path, vocab_size, special_tokens, **kwargs)
